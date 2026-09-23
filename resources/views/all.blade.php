43c6cadc1d4944aede59018c1eae8950b1031562
MaxDonder <macsven.donato-derouen@mmibordeaux.com>
2026-09-23 16:57:51 +0200

    redirect + buttons

--- resources/views/all.blade.php
+++ resources/views/all.blade.php
@@ -1,13 +1,13 @@
 <h1>Liste des Musiques</h1>
-<a href="{{ route('music.create') }}">Créer une nouvelle musique</a>
+<button><a href="{{ route('music.create') }}">Créer une nouvelle musique</a></button  >
 @foreach ($musics as $music)
     <h3>{{ $music->name }}</h3>
     <p>{{ $music->composer }}</p>
     <p>Game : {{ $music->game }}</p>
     <p>Précision : {{ $music->precision }}</p>
     <p>Lien : <a href="{{ $music->link }}" target="_blank">{{ $music->link }}</a></p>
-    <a href="{{ route('music.show', ['id' => $music->id]) }}">Voir la musique</a>
-    <a href="{{ route('music.edit.view', ['id' => $music->id]) }}">Modifier la musique</a>
-    <a href="{{ route('music.delete', ['id' => $music->id])}}">Supprimer la musique</a>
+    <button><a href="{{ route('music.show', ['id' => $music->id]) }}">Voir la musique</a></button>
+    <button><a href="{{ route('music.edit.view', ['id' => $music->id]) }}">Modifier la musique</a></button>
+    <button><a href="{{ route('music.delete', ['id' => $music->id])}}">Supprimer la musique</a></button>
     </br>
 @endforeach
--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -3,3 +3,7 @@
 <p>{{ $music->game }}</p>
 <p>Précision : {{ $music->precision }}</p>
 <p>Lien : <a href="{{ $music->link }}" target="_blank">{{ $music->link }}</a></p>
+</br>
+<button><a href="{{ route('music.list') }}">Retour à la liste des musiques</a></button>
+<button><a href="{{ route('music.edit.view', ['id' => $music->id]) }}">Modifier la musique</a></button>
+<button><a href="{{ route('music.delete', ['id' => $music->id])}}">Supprimer la musique</a></button>
