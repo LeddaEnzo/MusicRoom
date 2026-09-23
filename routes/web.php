cfc07a4301b835fac64326c5d805fac0790bc76d
MaxDonder <macsven.donato-derouen@mmibordeaux.com>
2026-09-23 12:30:54 +0200

    Routes>web

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Models\Music;
+use App\Http\Controllers\MusicController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -8,4 +11,28 @@ Route::get('/', function () {
 
 Route::get('/hello', function () {
     return 'Hello, World!';
+});
+
+Route::get('/music', function () {
+    return 'music';
+});
+
+
+Route::prefix('music')->name('music.')->group(function () {
+    //afficher music
+    Route::get('/', [MusicController::class, 'list'])->name('list');
+    Route::get('/{id}', [MusicController::class, 'show'])->where('id', '[0-9]+')->name('show');
+
+    //Créer musique
+    Route::get('/create', function () {
+        return view('music.create');
+    })->name('music.create');
+    Route::post('/create', [MusicController::class, 'create'])->name('music.create');
+
+    //Modifier musique
+    Route::get('/{id}/edit', [MusicController::class, 'edit_view'])->name('edit.view');
+    Route::post('/{id}/edit', [MusicController::class, 'edit'])->name('edit');
+
+    //Supprimer musique
+    Route::get('/{id}/delete', [MusicController::class, 'delete'])->name('delete');
 });
