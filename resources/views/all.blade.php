2296eb54add41dea081880835dcae227229e7f6e
enzo ledda <enzoledda92@gmail.com>
2026-09-24 14:18:37 +0200

    Updated front

--- resources/views/all.blade.php
+++ resources/views/all.blade.php
@@ -6,19 +6,25 @@
         <title>MusicRoom</title>
         <link rel="stylesheet" href="{{ asset('css/app.css') }}">
     </head>
-    <header>
-        <h1>Liste des Musiques</h1>
-        <button><a href="{{ route('music.create') }}">Créer une nouvelle musique</a></button>
-    </header>
+
     <body>
+        <header>
+            <h1>Liste des Musiques</h1>
+            <button><a href="{{ route('music.create') }}">Créer une nouvelle musique</a></button>
+        </header>
         <div class="music-grid">
             @foreach ($musics as $music)
                 <div class="music-card">
                     <h3>{{ $music->name }}</h3>
-                    <p>Compositeur : {{ $music->composer}}</p>
-                    <p>Jeu : {{ $music->game }}</p>
-                    <p>Précision : {{ $music->precision }}</p>
-                    <p>Lien : <a href="{{ $music->link }}" target="_blank">écouter la musique</a></p>
+                    <div class="music-info">
+                        <strong>Compositeur : </strong>
+                        <span>{{ $music->composer}}</span>
+                        <strong>Jeu : </strong>
+                        <span>{{ $music->game }}</span>
+                        <strong>Précision : </strong>
+                        <span>{{ $music->precision }}</span>
+                        <br>
+                    </div>
                     <div class="music-buttons">
                         <button><a href="{{ route('music.show', ['id' => $music->id]) }}">Voir la musique</a></button>
                         <button><a href="{{ route('music.edit.view', ['id' => $music->id]) }}">Modifier la musique</a></button>
--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -7,14 +7,16 @@
         <link rel="stylesheet" href="{{ asset('css/app.css') }}">
     </head>
     <body>
-        <h1>{{ $music->name }}</h1>
-        <h3>{{ $music->composer }}</h3>
-        <p>{{ $music->game }}</p>
-        <p>Précision : {{ $music->precision }}</p>
-        <p>Lien : <a href="{{ $music->link }}" target="_blank">{{ $music->link }}</a></p>
-        </br>
-        <button><a href="{{ route('music.list') }}">Retour à la liste des musiques</a></button>
-        <button><a href="{{ route('music.edit.view', ['id' => $music->id]) }}">Modifier la musique</a></button>
-        <button><a href="{{ route('music.delete', ['id' => $music->id])}}">Supprimer la musique</a></button>
+        <div class=music-show>
+            <h1>{{ $music->name }}</h1>
+            <p>Compositeur : {{ $music->composer }}</p>
+            <p>Jeu : {{ $music->game }}</p>
+            <p>Précision : {{ $music->precision }}</p>
+            <p>Lien : <a href="{{ $music->link }}" target="_blank">écouter la musique</a></p>
+            </br>
+            <button><a href="{{ route('music.list') }}">Retour à la liste des musiques</a></button>
+            <button><a href="{{ route('music.edit.view', ['id' => $music->id]) }}">Modifier la musique</a></button>
+            <button><a href="{{ route('music.delete', ['id' => $music->id])}}">Supprimer la musique</a></button>
+        <div>
     </body>
 </html>
